798f023adf4db0ee7e9e084f9ad78c9aa2eafc2f
Raquel <37589771+raquel2terrero@users.noreply.github.com>
2019-01-14 20:08:54 +0100

    update funcion resumen
    parametro para printar/no printar

--- resumen.m
+++ resumen.m
@@ -1,8 +1,15 @@
-function [accuracy,f1_score] = resumen(labels,pred)
+function [accuracy,f1_score] = resumen(labels,pred, quiet=1)
 %resumen resultados prediccion para clasificador binario
-    accuracy = sum(labels==pred) / length(labels)
-    C = confusionmat(labels,pred)
-    precision = C(2,2) / sum(C(:,2))
-    recall = C(2,2) / sum(C(2,:))
-    f1_score = 2 * precision * recall / (precision + recall)
+    accuracy = sum(labels==pred) / length(labels);
+    C = confusionmat(labels,pred);
+    precision = C(2,2) / sum(C(:,2));
+    recall = C(2,2) / sum(C(2,:));
+    f1_score = 2 * precision * recall / (precision + recall);
+    if quiet ~= 1
+        fprintf('Accuracy: %d\n', accuracy)
+        fprintf('Precision: %d\n', precision)
+        fprintf('Recall: %d\n', recall)
+        fprintf('F1-score: %d\n', f1_score)
+        C
+    end
 end
