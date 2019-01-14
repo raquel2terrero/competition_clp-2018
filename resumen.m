b34862c3d0db01ceba57c11fd1f32e54c990b70d
Raquel <37589771+raquel2terrero@users.noreply.github.com>
2019-01-14 20:39:15 +0100

    update resumen
    mejor formato

--- resumen.m
+++ resumen.m
@@ -9,10 +9,10 @@ function [accuracy,f1_score] = resumen(labels,pred, quiet)
     recall = C(2,2) / sum(C(2,:));
     f1_score = 2 * precision * recall / (precision + recall);
     if quiet ~= 1
-        fprintf('Accuracy: %d\n', accuracy)
-        fprintf('Precision: %d\n', precision)
-        fprintf('Recall: %d\n', recall)
-        fprintf('F1-score: %d\n', f1_score)
+        fprintf('Accuracy: %f\n', accuracy)
+        fprintf('Precision: %f\n', precision)
+        fprintf('Recall: %f\n', recall)
+        fprintf('F1-score: %f\n', f1_score)
         C
     end
 end
