fd5ed3dc1d610c4c34a708cd6bd562fb106e5f90
Raquel <37589771+raquel2terrero@users.noreply.github.com>
2019-01-14 21:21:39 +0100

    upload predict with ensamble
    predecir clase binaria con varios modelos por votacion

---
+++ predict_with_ensable.m
@@ -0,0 +1,11 @@
+function [pred] = predict_with_ensable(Mdl_ens,X)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+    pred = 0;
+    for i = 1:length(Mdl_ens)
+        pred = pred + predict(Mdl_ens{i}, X);
+    end
+    pred = pred / length(Mdl_ens);
+    pred = double(pred > 0.5);
+end
+
